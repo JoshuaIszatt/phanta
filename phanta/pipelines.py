bc16ebe015786a3fb60affe44328ad7f6b9e3f8d
hermes <joshiszatt@gmail.com>
2024-05-29 09:09:36 +0800

    from home

--- phanta/pipelines.py
+++ phanta/pipelines.py
@@ -1,3 +1,24 @@
 import os
+from .functions import (
+    interleave_reads
+)
 
 
+# _____________________________________________________PIPELINES
+
+
+def assembly_pipeline(input_file, output_dir, isolate='unknown', log=False):
+    pass
+
+# _____________________________________________________BATCHES
+
+
+def batch_assembly_pipeline(input_dir, output_dir, isolates="unknown"):
+    os.makedirs(output_dir, exist_ok=True)
+    for file in os.listdir(input_dir):
+        path = os.path.join(input_dir, file)
+        try:
+            assembly_pipeline(path, output_dir, isolates)
+        except Exception as e:
+            print(f"ERROR {e}")
+            continue
